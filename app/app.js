1a746a8b1f16154fd6bc1f010740f81f0fa9c788
vs4vijay <vs4vijay@gmail.com>
2015-09-07 21:55:38 +0530

    changes

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 /** TogetherJS Configuration */
 
-(function(TogetherJS) {
+;(function(TogetherJS) {
 
   var configs = {
     toolName: 'CoBrowse',
@@ -9,7 +9,7 @@
     cloneClicks: true,
     storagePrefix: 'cobrowse',
     //disableWebRTC: false,
-    getUserName: _getUserName,
+    //getUserName: _getUserName,
     suppressInvite: true,
     suppressJoinConfirmation: true
   };
@@ -20,18 +20,56 @@
   //TogetherJS.config('toolName', config.toolName);
 
   TogetherJS.on('ready', function() {
+    document.querySelector('#togetherjs-container').style.display = 'none';
     var shareUrl = TogetherJS.shareUrl();
-    document.getElementById('demoTxt').value = shareUrl;
-    document.getElementById('demoLabel').innerHTML = shareUrl;
-    console.log('Share URL', shareUrl);
+    _createCobrowseEvent(shareUrl);
+    _addStyle();
   });
 
-  var _getUserName = function(username) {
-    alert(username);
+  TogetherJS.on('close', function() {
+    var cobrowseEvent = document.createEvent('Event');
+    cobrowseEvent.initEvent('Cobrowse.ended', true, true);
+    console.info('[+] Cobrowse Session Ended');
+    _removeStyle();
+  });
+
+  var _createCobrowseEvent = function(shareUrl) {
+    var cobrowseEvent = document.createEvent('Event');
+    cobrowseEvent.shareUrl = shareUrl;
+    cobrowseEvent.initEvent('Cobrowse.started', true, true);
+    window.dispatchEvent(cobrowseEvent);
   };
 
+  var _addStyle = function() {
+    document.body.style.border = '2px dashed green';
+
+    window.cobrowseInterval = setInterval(function() {
+      var border = document.body.style.border;
+      if(border == '2px dashed green') {
+        document.body.style.border = '2px dashed red';
+      } else {
+        document.body.style.border = '2px dashed green';
+      }
+    }, 200);
+  };
+
+  var _removeStyle = function() {
+    document.body.style.border = '';
+
+    if(window.cobrowseInterval) {
+      clearInterval(window.cobrowseInterval);
+    }
+  };
+
+  window.addEventListener('Cobrowse.started', function(obj) {
+    console.log('start', obj);
+    document.getElementById('demoLabel').innerHTML = obj.shareUrl;
+    console.info('[+] Got Share URL', obj.shareUrl);
+  });
+
   window.demoClick = function(demoVar) {
-    console.log('demo click log');
+    document.getElementById('demoTxt').value = 'Button Clicked ' + Math.floor(Math.random() * 100);
+    console.info('demo click log');
   };
 
   window.startCobrowse = function(options) {
